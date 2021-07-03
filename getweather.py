0f3b22c72cde23d6991ca88aa07b18fc1c060881
Lukas Bukovan <olovopb@gmail.com>
2021-07-03 22:54:37 +0200

    getweather works

--- getweather.py
+++ getweather.py
@@ -9,7 +9,6 @@ try:
     API_KEY = os.environ['OPENWEATHER_API_KEY']
 except Exception:
     API_KEY = input('OPENWEATHER_API_KEY not exported in bash, please insert API key:')
-    exit(1)
 
 try:
     CITY = os.environ['CITY_NAME']
@@ -17,4 +16,11 @@ except Exception:
     CITY = input('CITY_NAME not exported in bash, please insert CITY_NAME: ')
 
 owm = pyowm.OWM(API_KEY)
+manager= owm.weather_manager()
+location = manager.weather_at_place(CITY).weather
+
+print('source=openweathermap, city={0}, description="{1}", temp="{2}",\
+humidity="{3}"'.format(CITY,location.status,location.temperature(unit="celsius")['temp']\
+,location.humidity))
+
 
